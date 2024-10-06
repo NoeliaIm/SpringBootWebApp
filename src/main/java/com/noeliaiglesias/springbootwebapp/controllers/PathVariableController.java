61f808bfccf5ac2e7b8947d4aecd6c02b9bfef6d
Noelia Iglesias <iglesias.noelia@yahoo.es>
2024-10-06 14:24:14 +0200

    value anotation

--- src/main/java/com/noeliaiglesias/springbootwebapp/controllers/PathVariableController.java
+++ src/main/java/com/noeliaiglesias/springbootwebapp/controllers/PathVariableController.java
@@ -2,11 +2,22 @@ package com.noeliaiglesias.springbootwebapp.controllers;
 
 import com.noeliaiglesias.springbootwebapp.models.User;
 import com.noeliaiglesias.springbootwebapp.models.dto.ParamDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/pathvariable")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+    @Value("${config.welcome.message}")
+    private String welcomeMessage;
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
 
     @GetMapping("/details/{name}/{city}/{cp}")
     public ParamDto details(@PathVariable String name, @PathVariable String city, @PathVariable int cp) {
@@ -24,5 +35,10 @@ public class PathVariableController {
         userCreated.setCity(user.getCity().toUpperCase());
         userCreated.setCp(user.getCp());
         return userCreated;
+    }
+
+    @GetMapping("/config")
+    public Map<String, Object> getConfig(@Value("${config.password}") String password, @Value("${config.code}") int code) {
+        return Map.of("username", username, "welcomeMessage", welcomeMessage, "listOfValues", listOfValues, "password", password, "code", code);
     }
 }
